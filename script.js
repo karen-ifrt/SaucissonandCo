1ce56cfb8e10364470324822081b7ba425351d09
karen-ifrt <karen-ifrt@users.noreply.github.com>
2019-10-25 16:35:29 +0200

    Responsive

---
+++ script.js
@@ -0,0 +1,18 @@
+// myproduct = document.getElementById("products");
+
+// myproduct.addEventListener("click", function(e){
+//     e.preventDefault();
+//     $("navbar").animate({ scrollTop: "myproduct" }, "600");
+// })
+
+
+// $(function(){
+    
+//     $(".navbar a").on("click", function(e){
+       
+//         e.preventDefault();
+//         var hash = this.hash;
+        
+//         $('body,html').animate({scrollTop: $(hash).offset().top} , 900 , function(){window.location.hash = hash;})
+        
+//     });
