fdb8da0bbf98d017122a0ab3062c3550d337308b
karen-ifrt <karen-ifrt@users.noreply.github.com>
2019-10-28 09:27:56 +0100

    v2

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-// myproduct = document.getElementById("products");
+// navigation éléments navbar
 
-// myproduct.addEventListener("click", function(e){
-//     e.preventDefault();
-//     $("navbar").animate({ scrollTop: "myproduct" }, "600");
-// })
+$(function () {
 
+    $(".navbar a").on("click", function (e) {
 
-// $(function(){
-    
-//     $(".navbar a").on("click", function(e){
-       
-//         e.preventDefault();
-//         var hash = this.hash;
-        
-//         $('body,html').animate({scrollTop: $(hash).offset().top} , 900 , function(){window.location.hash = hash;})
-        
-//     });
+        e.preventDefault();
+        let hash = this.hash;
+
+        $('html').animate({ scrollTop: $(hash).offset().top }, 800, function () { window.location.hash = hash; });
+
+    });
+
+})
+
+// bouton retour en haut
+
