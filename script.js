ce944b52a824843ef1dd4bc266ee4256cec7bef2
karen-ifrt <karen-ifrt@users.noreply.github.com>
2019-10-28 11:38:04 +0100

    v3

--- script.js
+++ script.js
@@ -15,3 +15,17 @@ $(function () {
 
 // bouton retour en haut
 
+mybutton = document.getElementById("myBtn");
+
+window.addEventListener("scroll", function(){
+  if (document.documentElement.scrollTop > 200) {
+    mybutton.style.bottom = "30px";
+
+  } else {
+    mybutton.style.bottom = "-100px";
+  }
+});
+
+function topFunction() {
+    $("html").animate({ scrollTop: "0" }, "600");
+}
